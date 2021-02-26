cf51304abe99b302d50815e83160deaf2d59e3d5
gary yang <garyyang1688@gmail.com>
2021-02-26 18:21:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+if dif == "Customer Support":
+  st = input(
+"What type of support do you need?\n 1. \n 2. \n 3.Write a Review "
+)
+if dif == "Buy a Book":
+  st = input(
+"What book do you want to read?\n 1. Battle of the oven\n 2. The mega oven\n 3. "
+)
+if dif == "Write a review":
+  st = print(
+"Ok. If so, then fill out this form. Try entering this \nlink. P.S: You might have problems using the link. https://docs.google.com/forms/d/e/1FAIpQLSfvMeBGGaZE_vsgYZz-ZQHne7i6sWSBY_n785tVuXATAG766w/viewform")
+  print("Welcome to the Potato and Fries Official Website. How can we \nhelp you today?")
+dif = input("\n 1. Customer Support\n 2. Buy a Book\n 3. Write a Book")
